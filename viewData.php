0a9df4de2fbbe8c505ba8cfc3617b6f632ac6cd2
Omar Saiouf <homar0083@gmail.com>
2025-04-22 20:11:42 +0300

    session

---
+++ viewData.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Data</title>
+</head>
+<body>
+    <h1>Data :</h1>
+    <h2>username:</h2><?= $_SESSION['username'] ?><br>
+    <h2>email:</h2><?= $_SESSION['email'] ?><br>
+    <h2>age:</h2><?= $_SESSION['age'] ?><br>
+    <h2>password:</h2><?= $_SESSION['password'] ?><br>
+
+</body>
+</html>
